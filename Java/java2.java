f5656534887bf4d23af332dbb9c5e9e5663ef380
TechSarthak17 <2007sarthak17@gmail.com>
2026-04-03 23:58:14 +0530

    Java 2 Done Logic bulid

---
+++ Java/java2.java
@@ -0,0 +1,18 @@
+package Java;
+import java.nio.channels.Pipe.SourceChannel;
+import java.util.Scanner;
+
+public class java2 {
+
+    public static void main (String[]arg){
+        Scanner Ok = new Scanner(System.in);
+        System.out.print("Enter Your First Number ");
+        int a = Ok.nextInt();
+        System.out.print("Enter Your 2nd Number ");
+        int b = Ok.nextInt();
+
+        int sum = a + b ;
+        System.out.print("Your sum is there ");
+        System.out.println(sum);
+    }
+}
