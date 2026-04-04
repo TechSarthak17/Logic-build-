326be4f920cdc744906994aa59369e7314cc6271
TechSarthak17 <2007sarthak17@gmail.com>
2026-04-05 04:36:46 +0530

    Sum of First N Numbers 2

--- Java/Day2/java2.java
+++ Java/Day2/java2.java
@@ -1,8 +1,6 @@
 package Java.Day2;
 import java.util.Scanner;
 
-import javax.annotation.processing.SupportedOptions;
-
 public class java2 {
     public static void main (String[]args){
         Scanner sc = new Scanner(System.in);
