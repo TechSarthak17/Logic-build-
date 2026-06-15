7dac4db62a970d3d529292ad46fbdb8ce53e3a8a
TechSarthak17 <2007sarthak17@gmail.com>
2026-06-15 10:20:04 +0530

    Sum of first revision is done

--- Java/Check/Check.java
+++ Java/Check/Check.java
@@ -1,3 +1,23 @@
 //Sum of First Number 
 
 package Java.Check;
+import java.util.Scanner;
+class Check {
+    public static void main (String[]arg){
+        Scanner sc = new Scanner(System.in);
+        System.err.println("Enter a number");
+
+        if(sc.hasNext()){
+
+            
+            int a = sc.nextInt();
+            int sum = 0;
+
+            for (int i = 0 ; i<=a ;i++){
+                sum = sum+ i ;
+            }
+            System.out.println("Your Out put is " + sum + "");
+        }
+        sc.close();
+    }
+}
