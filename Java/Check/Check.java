74f51d23954a817d196e719a012ff758e08420d7
TechSarthak17 <2007sarthak17@gmail.com>
2026-06-15 10:06:30 +0530

    Factorial number is done revision

--- Java/Check/Check.java
+++ Java/Check/Check.java
@@ -1,26 +1,3 @@
+//Sum of First Number 
+
 package Java.Check;
-import java.util.Scanner;
-
-class check {
-
-    public static void main (String[]arg){
-
-        Scanner sc = new Scanner(System.in);
-
-        if(sc.hasNext()){
-
-            int a = sc.nextInt();
-
-            for(int i=0 ;i<=5 ;i++ ){
-
-                System.out.println(i);
-
-            }
-
-        }else {
-            System.out.println("Invalid input");
-        }sc.close();
-    
-    }
-
-}
