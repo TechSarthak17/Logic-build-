ee621756d777dfdf0644cf5d0d8252e39e742ab9
TechSarthak17 <2007sarthak17@gmail.com>
2026-06-20 13:17:35 +0530

    Learning from this Add objects

--- Java/Core/test8.java
+++ Java/Core/test8.java
@@ -1,20 +1,20 @@
 package Java.Core;
 
 public class test8 {
-    String name ;
+    String name;
     String address;
-    int age ;
-    float weight ;
-    float Hight ;
+    int age;
+    float weight;
+    float Hight;
     int moblie;
     String HarirColour;
-    String Gender ;
+    String Gender;
 
     int eye;
     int ear;
-    int  ok;
+    int ok;
 
-    public void eat () {
+    public void eat() {
 
     }
 
@@ -22,7 +22,7 @@ public class test8 {
 
     }
 
-    public void run(){
+    public void run() {
 
     }
 
@@ -30,20 +30,38 @@ public class test8 {
 
     }
 
-    public void see(){
+    public void see() {
 
-        if(this.eye == 2){
-            System.out.println("I can see clearly ");
+        if (this.eye == 2) {
+            System.out.println("I can see clearly");
         }
 
-        if(this.eye == 1){
+        if (this.eye == 1) {
             System.out.println("See thoda thoda");
         }
 
-        if (this.eye == 0 ){
-            System.out.println("Can't see any thing");
+        if (this.eye == 0) {
+            System.out.println("Can't see anything");
         }
     }
 
-    
+    public static void main(String[] args) {
+
+        // Creating object
+        test8 person1 = new test8();
+
+        // Assigning values
+        person1.name = "Sarthak";
+        person1.age = 19;
+        person1.weight = 64.5f;
+        person1.eye = 2;
+
+        // Calling method
+        person1.see();
+
+        // Printing data
+        System.out.println("Name: " + person1.name);
+        System.out.println("Age: " + person1.age);
+        System.out.println("Weight: " + person1.weight);
+    }
 }
