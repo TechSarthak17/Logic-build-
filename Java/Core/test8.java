c4cd05e6f612033d0f40bac0a29297b6af5ae40e
TechSarthak17 <2007sarthak17@gmail.com>
2026-06-20 13:09:38 +0530

    Learning from this 2

---
+++ Java/Core/test8.java
@@ -0,0 +1,49 @@
+package Java.Core;
+
+public class test8 {
+    String name ;
+    String address;
+    int age ;
+    float weight ;
+    float Hight ;
+    int moblie;
+    String HarirColour;
+    String Gender ;
+
+    int eye;
+    int ear;
+    int  ok;
+
+    public void eat () {
+
+    }
+
+    public void sleep() {
+
+    }
+
+    public void run(){
+
+    }
+
+    public void hear() {
+
+    }
+
+    public void see(){
+
+        if(this.eye == 2){
+            System.out.println("I can see clearly ");
+        }
+
+        if(this.eye == 1){
+            System.out.println("See thoda thoda");
+        }
+
+        if (this.eye == 0 ){
+            System.out.println("Can't see any thing");
+        }
+    }
+
+    
+}
